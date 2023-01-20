1f6abf37789c3bdd9883bb81050286b4c5956983
dac <dac@dypm.org>
2023-01-20 22:37:19 +0530

    comit

---
+++ mangesh/BOL/Department.cs
@@ -0,0 +1,7 @@
+﻿namespace BOL;
+public class Department
+{
+public int Id {get;set;}
+public string? Name {get;set;}
+public string? Location {get;set;}
+}
---
+++ mangesh/suhasApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace suhasApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
